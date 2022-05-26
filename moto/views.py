88f65a5579026463a7f29a5ffc7d5a5b91e39139
Benjamin <benj.james.carter@gmail.com>
2022-05-26 13:24:53 -0700

    completed models and views

--- moto/views.py
+++ moto/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Moto
+from .serializers import MotoSerializer
 
-# Create your views here.
+
+class MotoList(generics.ListCreateAPIView):
+    queryset = Moto.ojects.all()  # when you call the API, you will get all of the motos
+    serializer_class = MotoSerializer
+
+
+class MotoDetail(generics.RetrieveUpdateAPIView):
+    queryset = Moto.objects.all()
+    serializer_class = MotoSerializer
